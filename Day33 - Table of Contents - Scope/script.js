d6c8f7d2c3a6ab75e91b966d1dcd2bf87446c04a
Wiktor Tuminski <wiktor.tuminski@trip4share.com>
2020-11-13 09:45:48 +0100

    day 33 - keeping functions and variables out of the global scope:

---
+++ Day33 - Table of Contents - Scope/script.js
@@ -0,0 +1,20 @@
+(createTable = () => {
+    const divTable = document.querySelector("#table-of-contents");
+    const h2 = Array.prototype.slice.call(document.querySelectorAll("h2"));
+
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+    tbody.innerHTML += `<thead>
+                            <tr>
+                                <th>List of anchor links</th>
+                            </tr>
+                        </thead>`;
+
+    h2.forEach((item, index )=> {
+        item.id = `section${index}`
+        tbody.innerHTML += `<tr><a href="#${item.id}">${item.textContent}</a></tr>`;
+    });
+
+    table.appendChild(tbody);
+    divTable.appendChild(table);
+})();
