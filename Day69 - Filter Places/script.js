cb0d80a4ebd18a9e60c868e00845a2fa9ec555d7
Wiktor Tuminski <wiktor.tuminski@trip4share.com>
2021-01-25 17:08:36 +0100

    cleaning

--- Day69 - Filter Places/script.js
+++ Day69 - Filter Places/script.js
@@ -34,36 +34,40 @@ Reef.prototype.convertString = (word) =>{
         case "no": case "false": case "0": case null: return false;
         default: return Boolean(word);
     }
-}
+};
+
+Reef.prototype.filterPlaces = function(data){
+    return data.filter(item => {
+        const faves = this.convertString(this.data.favorites[item.id]);
+        const visit = this.convertString(this.data.visited[item.id]);
+
+        switch (this.data.filter) {
+            case filters.visited:
+                return visit === true;
+            case filters.notVisited:
+                return visit !== true;
+            case filters.faves:
+                return faves === true;
+            default:
+                return item;
+        };}
+    );
+};
 
 Reef.prototype.createCards = function(props){
     const _favorites = this.data.favorites || {};
     const _visited = this.data.visited || {};
-    return props
-        .filter(item => {
-            const faves = this.convertString(this.data.favorites[item.id]);
-            const visit = this.convertString(this.data.visited[item.id]);
-
-            switch (this.data.filter) {
-                case filters.visited:
-                    return visit === true;
-                case filters.notVisited:
-                    return visit !== true;
-                case filters.faves:
-                    return faves === true;
-                default:
-                    return item;
-            };
-        })
+    const filteredPlaces = this.filterPlaces(props) || [];
+    return filteredPlaces
         .map(item => {
             return `<div class="card" id="${item.id}">
                         <header>
                             <h2>${item.place}</h2>
                             <div class="buttons">
-                                <button class="${favorites}" aria-label="add ${item.place} to favorties" aria-pressed="${_favorites[item.id] || false}">
-                                    <i class="fas fa-heart">
-                                </i></button>
-                                <button class="${visited}" aria-label="mark ${item.place} as visited" aria-pressed="${_visited[item.id] || false}">
+                                <button data-type="${favorites}" aria-label="add ${item.place} to favorties" aria-pressed="${_favorites[item.id] || false}">
+                                    <i class="fas fa-heart"></i>
+                                </button>
+                                <button data-type="${visited}" aria-label="mark ${item.place} as visited" aria-pressed="${_visited[item.id] || false}">
                                     <i class="fas fa-check-square"></i>
                                 </button>
                             </div>
@@ -83,34 +87,19 @@ Reef.prototype.createCards = function(props){
 };
 
 Reef.prototype.handleClick = function(e){
-    //toggleFavourites
-    if(e.target.closest(`button.${favorites}`)?.hasAttribute("aria-pressed")){
-        
-        const id = e.target.closest(".card").id;
+
+    //toggle Favourites and Visited
+    if(e.target.closest('button')?.hasAttribute("aria-pressed")){
+        const ID = e.target.closest(".card").id;
+        const type = e.target.closest("button").dataset.type;
 
         //toggle aria-pressed
         const newVal = e.target.closest("button").getAttribute('aria-pressed') === 'true' ? 'false' : 'true';
-        this.data[favorites][id] = newVal;
+        this.data[type][ID] = newVal;
 
         //save to localstorage
-        this.saveToLocalStorage(favorites, JSON.stringify(this.data[favorites]));
-        
+        this.saveToLocalStorage(type, JSON.stringify(this.data[type]))
     };
-
-    //toggleVisited
-    if(e.target.closest(`button.${visited}`)?.hasAttribute("aria-pressed")){
-        
-        const id = e.target.closest(".card").id;
-
-        //toggle aria-pressed
-        const newVal = e.target.closest("button").getAttribute('aria-pressed') === 'true' ? 'false' : 'true';
-        this.data[visited][id] = newVal;
-
-        //save to localstorage
-        this.saveToLocalStorage(visited, JSON.stringify(this.data[visited]));
-        
-    };
-
 };
 
 Reef.prototype.handleFiltering = function(e){
@@ -123,8 +112,8 @@ Reef.prototype.saveToLocalStorage = function(key, value){
     localStorage.setItem(key, value);
 };
 
-Reef.prototype.getFromLocalStorage = function(prop, key){
-    this.data[prop] = JSON.parse(localStorage.getItem(key)) || {};
+Reef.prototype.getFromLocalStorage = function(key){
+    this.data[key] = JSON.parse(localStorage.getItem(key)) || {};
 };
 
 //
@@ -144,7 +133,7 @@ const app = new Reef('#app', {
     }
 });
 app.getData();
-app.getFromLocalStorage(favorites, favorites);
-app.getFromLocalStorage(visited, visited);
-document.addEventListener("click", app.handleClick.bind(app));
+app.getFromLocalStorage(favorites);
+app.getFromLocalStorage(visited);
+document.addEventListener("click", app.handleClick.bind(app), true);
 document.addEventListener("change", app.handleFiltering.bind(app));
